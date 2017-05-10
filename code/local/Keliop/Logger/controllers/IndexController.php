a65f152e0d03b6e2a991a9a654181737084e698d
langi <dawidziol14@wp.pl>
2017-05-10 23:59:52 +0200

    Add logger module

---
+++ code/local/Keliop/Logger/controllers/IndexController.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Description of indexController
+ *
+ * @author Dawid
+ */
+
+class Keliop_Logger_IndexController extends Mage_Core_Controller_Front_Action {        
+    public function indexAction() {
+        echo 'test';
+    }
+}
