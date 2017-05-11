990176528e2255e38c7a24cadef65ec0a2b30c35
langi <dawidziol14@wp.pl>
2017-05-11 15:29:21 +0200

    Add grid

--- code/local/Keliop/Logger/controllers/LoggerController.php
+++ code/local/Keliop/Logger/controllers/LoggerController.php
@@ -1,9 +1,19 @@
 <?php
-class Keliop_Logger_LoggerController extends Mage_Adminhtml_Controller_Action
-{
-    public function indexAction()
-    {
+
+/**
+ * Controller generate grid
+ * @author dawid
+ */
+class Keliop_Logger_LoggerController extends Mage_Adminhtml_Controller_Action {
+
+    /**
+     * Action display grid in adminhtml
+     */
+    public function indexAction() {
+        
         $this->loadLayout();
+        $this->_addContent($this->getLayout()->createBlock('keliop_logger/adminhtml_logger_grid'));
         $this->renderLayout();
     }
+
 }
