4ea497e557cb4301d80467b0834386a7da3a7ce9
langi <dawidziol14@wp.pl>
2017-05-16 13:39:03 +0200

    Set up logger block with layout file

--- code/local/Kaliop/Logger/controllers/LoggerController.php
+++ code/local/Kaliop/Logger/controllers/LoggerController.php
@@ -10,10 +10,9 @@ class Kaliop_Logger_LoggerController extends Mage_Adminhtml_Controller_Action {
      * Action display grid in adminhtml
      */
     public function indexAction() {
-        
+
         $this->loadLayout();
         $this->_setActiveMenu('kaliop');
-        $this->_addContent($this->getLayout()->createBlock('kaliop_logger/adminhtml_logger_grid'));
         $this->renderLayout();
     }
 
